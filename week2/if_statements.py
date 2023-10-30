f4d24b750cba2760f777b2836435e2f666e57db8
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-30 17:31:28 +0000

    Added code to demonstrate the use of comparison operators.

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -39,3 +39,17 @@ else:
     print(f"The number {num} is an even number.")
 
 # there is no need to write if num % 2 == 1 , 1 means the value of boolean it true, 0 is false.
+
+#########################################
+Activity 5: Comparison Operators
+
+print("Please enter the first number.")
+num1 = int(input())
+print("Please enter the second number.")
+num2 = int(input())
+if num1 > num2:
+    print("The second number is the smallest.")
+elif num1 < num2:
+    print("The first number is the smallest.")
+else:
+    print("Both are equal!")

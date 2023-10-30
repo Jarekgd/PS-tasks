ddf5d586dccb8d968ae0ae2271fdb4273b9faf1a
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-30 15:26:14 +0000

    Added code to demonstrate the use of different data types.

--- week1/input_tasks.py
+++ week1/input_tasks.py
@@ -1,3 +1,4 @@
+# Activity 1
 # Ask user to enter their name
 
 print("What is your name?")
@@ -7,6 +8,7 @@ print(f"It is nice to meet you {name}")
 # name = input("What is your name?")
 # print(f"It is nice to meet you {name}")
 
+# Activity 2: ASCII Robot
 print("\nPlease enter a character for the eye")
 eye = input()
 print("")
@@ -19,3 +21,35 @@ print("#         #")
 print("#  =====  #")
 print("###########")
 print("   #  #")
+
+# Activity 3: Data Types
+print("\nWhat is your name?")
+name = input()
+print("How old are you?(in years)")
+age = int(input())
+print("How tall are you (in meters)?")
+height = float(input())
+print("How much do you weigh (in kilograms)?")
+weight = int(input())
+bmi = weight / (height**2)
+print(f"{name} you are {age} years old and your BMI is {bmi:.2f}")
+
+# parenthesis are not required, exponent has priority before divide and multiply, but it is good for more readable  code
+
+# exponents can be done in different ways:
+# a = pow(height,2)
+# or
+# import math
+# a = math.pow(height,2)
+# or
+# import numpy as np
+# a = np.power(height, 2)
+
+# other way of formatting floats:
+# round(a, 2)
+# or
+# bmi_formated = "{:.2f}".format(bmi)
+# or
+# bmi_formated = format(bmi, ".2f")
+
+

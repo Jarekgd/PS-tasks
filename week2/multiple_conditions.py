3d4c66742bb9cf67ab5cc90fd069eae687570fa5
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-30 18:38:04 +0000

    Added code to demonstrate the use of a logical and operator.

--- week2/multiple_conditions.py
+++ week2/multiple_conditions.py
@@ -9,4 +9,14 @@ elif (type == "safe") or (type == "long"):
 else:
     print("Not sure which route to take." )
 
+####################################
+# Activity 2: Logical AND operator
 
+print("What did I hear?")
+hear = input()
+print("\nWhat did I see?")
+see = input()
+if (hear == "grrr") and (see == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue!")

82ee031fdccedf0a6556f716fab491630314800e
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-30 19:03:03 +0000

    Added code to review the use of decisions, nesting and logical operators.

--- week2/multiple_conditions.py
+++ week2/multiple_conditions.py
@@ -20,3 +20,33 @@ if (hear == "grrr") and (see == "two red eyes"):
     print("There is a scary creature. I should get out of here!")
 else:
     print("I am a little scared but I will continue!")
+
+################################
+# Activity 3: Review
+
+print("Pet Store")
+print("Do you have a pet?(y/n)")
+doYou = input()
+if doYou == "y":
+    print("How many pets do you have?")
+    howMany = int(input())
+    if howMany > 1:
+        print(f"You have {howmany} pets! Wow!")
+        print("What pets do you have? (dogs/ cats/ fish")
+        whatKind = input()
+        if (whatKind == "dogs") or ( whatKind == "cats"):
+            print(f"I love {}! They are good friends")
+        elif whatKind == "fish":
+            print(f"I love fish. They don't bark!")
+    elif howMany == 1:
+        print("One is good, but buy more!")
+        print("What do you have?(dog/cat/fish")
+        whatKind = input()
+        if (whatKind == "dog") or ( whatKind == "cat"):
+            print(f"I love {}! They are good friends")
+        elif whatKind == "fish":
+            print("you have only one fish? What a shame!")
+    else:
+        print("C'mon! You should get a pet!")
+else:
+    print("You must have a pet! You live will be better!")

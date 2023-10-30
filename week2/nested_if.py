7f958c1fd92ced2b7bb891a487681d3c413773f0
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-30 18:21:50 +0000

    Added code to demonstrate the use of multiple nested statements.

--- week2/nested_if.py
+++ week2/nested_if.py
@@ -16,3 +16,29 @@ else:
 ########################################################
 # Activity 2: Multiple Nested Decisions
 
+print("Where should I look?")
+room = input()
+if room == "in the bedroom":
+    print("Where in the bedroom should I look?")
+    place = input()
+    if place == "under the bed":
+        print("Found some shoes but no phone")
+    else:
+        print("Found some mess but no phone.")
+elif room == "in the bathroom":
+    print("Where in the bathroom should I look?")
+    place = input()
+    if place == "in the bathtub" :
+        print("Found a rubber duck but no phone")
+    else:
+        print("Found bathroom stuff but no phone.")
+
+elif room == "in the living room":
+    print("Where in the living room should I look?")
+    place = input()
+    if place == "in the bathtub" :
+        print("Yes! I found my phone!")
+    else:
+        print("Found some stuff but no phone.")
+else:
+    print("I do not know where that is but I will keep looking.")

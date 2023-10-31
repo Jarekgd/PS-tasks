d90f7eb24053dd31dba839c548494308a207c484
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-31 11:12:39 +0000

    Added code to demonstrate the use of a simple for loop.

--- week3/for_loops.py
+++ week3/for_loops.py
@@ -19,3 +19,26 @@ for i in range(distance, 0 , -1):
     print(f"{i} steps remaining")
 
 print("\nTarget achieved!")
+
+###################################
+# Activity 3: Range
+
+level = int(input("What level of brightness is required?\n"))
+print("Adjusting brightness...\n")
+
+for i in range(0, level + 1, 2):
+    print("*" * i)
+
+print("Complete!")
+
+###################################
+# Activity 4: Characters
+
+print("\nWhat word do you see?")
+word = input()
+print("Displaying index positions...\n")
+
+for i in range(0, len(word), 1):
+    print(f"index {i}: {word[i]}")
+
+print("\nDone!")

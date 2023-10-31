5bb68840302647dbb3f1abed0b19c9d7a45f2b50
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-10-31 14:00:59 +0000

    Added code to demonstrate the use of one loop nested is another.

--- week3/nested_loops.py
+++ week3/nested_loops.py
@@ -4,8 +4,8 @@ print("How many rows should I have?")
 rows = int(input())
 print("\nHow many columns should I have?")
 cols = int(input())
-print("\nHere I go:")
-for i in range(rows, 0, 1):
-    for j in range(cols,0,1):
-        print(":-)", end="")
+print("\nHere I go:\n")
+for i in range(0, rows, 1):
+    for j in range(0, cols, 1):
+        print(":-) ", end="")
     print("")

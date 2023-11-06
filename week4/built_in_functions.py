497735f82798b297f89d76afe452dd9dc3136c90
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-06 20:43:18 +0000

    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- week4/built_in_functions.py
+++ week4/built_in_functions.py
@@ -1,8 +1,23 @@
+# Activity 1: ASCII Value
+
 print("built_in_functions.py")
 letter = input("Please enter a letter:\n")
-
+value = ord(letter)
 if len(letter) == 1:
-    print(f"The ASCII code for {letter} is {ord(letter)}.")
+    print(f"The ASCII code for {letter} is {value}.")
 else:
     print("Too many letters!")
+print("Program Ended!\n")
+
+########################################
+# Activity 2: ASCII character
+
+print("Program started!")
+print("Please enter an ASCII code:")
+code = abs(int(input()))
+character = chr(code)
+if code in range(32,127):
+    print(f"The character represented by the ASCII code {code} is: {character}")
+else:
+    print("Wrong value!")
 print("Program Ended!")

b8c75826b1ff860c9d3c9871f7f7a7a7ac4069ea
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-06 21:14:59 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -2,6 +2,19 @@
 def listen():
     print("What sound did you hear?")
     sound = input()
-    print(f"That was a loud {sound}!")
+    print(f"That was a loud {sound}!\n")
 
 listen()
+
+#########################################
+# Activity 2: Nesting
+
+def identify ():
+    print("What lies before us?")
+    answer = input()
+    if answer == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+identify()

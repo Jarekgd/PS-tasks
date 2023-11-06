ba0f26bead4e9f4187d753c01acca1e0c6adb953
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-06 20:29:01 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

--- week4/built_in_functions.py
+++ week4/built_in_functions.py
@@ -1,1 +1,8 @@
 print("built_in_functions.py")
+letter = input("Please enter a letter:\n")
+
+if len(letter) == 1:
+    print(f"The ASCII code for {letter} is {ord(letter)}.")
+else:
+    print("Too many letters!")
+print("Program Ended!")

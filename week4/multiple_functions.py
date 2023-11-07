67f58fc39ca3eb2f51aa2e6c0c5a65a9d2c9ae93
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-07 08:58:46 +0000

    Added code to demonstrate the use of return values.

--- week4/multiple_functions.py
+++ week4/multiple_functions.py
@@ -12,3 +12,27 @@ def create_ladder():
     display_ladder(steps)
 
 create_ladder()
+
+#############################################
+# Activity 2: Return Values
+
+def sum_weights(char_w, invent_w):
+    return char_w + invent_w
+
+def calc_avg_weight(char_w, invent_w):
+    return sum_weights(char_w, invent_w) / 2
+
+def run():
+    print("What is the weight of the person?")
+    char_w = float(input())
+    print("What is the weight of their inventory?")
+    invent_w = float(input())
+    print("What would you like to calculate (sum or average)?")
+    answer = input()
+    calc_avg_weight(char_w, invent_w)
+    if answer == "sum":
+        print(f"The sum of weights is {sum_weights(char_w, invent_w)}.")
+    if answer == "average":
+        print(f"The average of weights is {calc_avg_weight(char_w, invent_w)}")
+
+run()

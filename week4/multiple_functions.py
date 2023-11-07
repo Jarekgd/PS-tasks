3bfc4326a4ea8954b181070d04a8b848b6fb455f
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-07 10:30:06 +0000

    Added code to demonstrate the use of multiple function calls.

--- week4/multiple_functions.py
+++ week4/multiple_functions.py
@@ -32,10 +32,65 @@ def run():
 
     suma = sum_weights(char_w, invent_w)
     aver = calc_avg_weight(char_w, invent_w)
-    
+
     if answer == "sum":
         print(f"The sum of weights is {suma}.")
     if answer == "average":
         print(f"The average of weights is {aver}")
 
 run()
+
+#################################################
+# Activity 3: Word Manipulation
+def box(word):
+    print(f"******{"*" * len(word)}*******")
+    print(f"*     {" " * len(word)}      *")
+    print(f"*     {word}      *")
+    print(f"*     {" " * len(word)}      *")
+    print(f"******{"*" *  len(word)}*******")
+
+def lowercase(word):
+    print(word.lower())
+
+def uppercase(word):
+    print(word.upper())
+
+def mirror(word):
+    reversed = ""
+    for i in word:
+        reversed = i + reversed
+    print(f"{word} | {reversed}")
+
+def repeat(word):
+    print("How many times?")
+    repeat = int(input())
+    for i in range(repeat):
+        if i % 2:
+            print(lowercase(word))
+        else:
+            print(uppercase(word))
+def run():
+    print("Enter a word:")
+    word = input()
+    print("Choose the option:")
+    print("1) Display in a Box")
+    print("2) Display Lower-case")
+    print("3) Display Upper-case")
+    print("4) Display Mirrored")
+    print("5) Repeat –  how many times:")
+    choice = input()
+    if choice == "1":
+        box(word)
+    elif choice == "2":
+        lowercase(word)
+    elif choice == "3":
+        uppercase(word)
+    elif choice == "4":
+        mirror(word)
+    elif choice == "5":
+        repeat(word)
+    else:
+        print("Wrong input")
+run()
+
+

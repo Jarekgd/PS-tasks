284e75e9b288cfcca5692d91eb5943f5bcc734bf
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-13 12:34:06 +0000

    Added code to demonstrate how to index a list.

--- week5/list_tasks.py
+++ week5/list_tasks.py
@@ -1,3 +1,4 @@
+# Activity 1: Simple List
 def directions():
     steps=["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
     return steps
@@ -7,3 +8,22 @@ def run_task1():
 
 if __name__ == "__main__":
     run_task1()
+
+####################################
+# Activity 2: Indexing
+
+def movements():
+    path=["Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1]
+    return path
+
+def run_task2():
+    print("Moving...")
+    path = movements()
+
+    for i in range(0,len(path),2):
+        direction = path[i]
+        steps = path[i + 1]
+        print(f"{direction} for {steps} steps")
+        i += i
+
+run_task2()

aae080d6209d43f37abf9bb645c323b3f73df1f7
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-24 11:28:26 +0000

    Added code to extract data from the CSV file.

--- week6/csv_tasks.py
+++ week6/csv_tasks.py
@@ -1,5 +1,4 @@
 # Activity 1: Reading a CSV File
-
 import csv
 
 def read_csv(path):
@@ -14,3 +13,25 @@ def read_csv(path):
 
 if __name__=="__main__":
     read_csv("clothing.csv")
+
+######################################
+# Activity 2: Extract Values from CSV
+print("")
+def extract(path):
+    print("Extracting... ",end="")
+    with open(path) as file:
+        csv_reader=csv.reader(file)
+        headings=next(csv_reader)
+        names=""
+        for value in csv_reader:
+            names+=value[1]+"\n"
+    print("Done!")
+    print("Done! The extracted items are:")
+    print(names)
+
+def run_task2():
+    extract("clothing.csv")
+
+if __name__=="__main__":
+    run_task2()
+

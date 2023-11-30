6b3cc3b48978867792c8886c39c8b3ab1ead7fae
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-30 20:18:37 +0000

    Added code to demonstrate how to manipulate and sort a set.

--- week7/set_tasks.py
+++ week7/set_tasks.py
@@ -3,6 +3,12 @@ def observed():
     observations = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
     return observations
 
+def run_task1():
+    s = observed()
+    print(s)
+
+run_task1()
+
 
 #################################
 # Activity 2: Set From Other Data Structures
@@ -21,14 +27,59 @@ def observed_items1():
 
 
 def run_task2():
-    print("\nCounting observations...")
+    print("\nCounting observations ...")
     ob = observed_items()
     s = set()
     for i in ob:
         s.add((i, ob.count(i)))
-    print("")
+    print()
     for i in s:
         print(f"{i[0]} observed {i[1]} times.")
 
 
 run_task2()
+
+###################################
+# Activity 3: Sorting
+
+def observed_items():
+    observations = []
+    print("Counting observations ...")
+    for i in range(5):
+        print("Please enter an observation:")
+        observations.append(input())
+    return observations
+
+
+def remove_observations():
+    ob = observed_items()
+    while True:
+        print("Do you wish to remove an observation (y/n)?")
+        choice = input()
+        if choice == "y":
+            print("What observation do you wish to remove?")
+            print(ob)
+            choice1 = input()
+            ob.remove(choice1)
+            print("Done!")
+            print(ob)
+        else:
+            break
+    return ob
+
+
+def run_task3():
+    rem = remove_observations()
+    print("Observations:")
+    s = set()
+    for i in rem:
+        s.add((i, rem.count(i)))
+    print()
+    for i in s:
+        print(f"{i[0]} observed {i[1]} times.")
+
+
+run_task3()
+
+
+

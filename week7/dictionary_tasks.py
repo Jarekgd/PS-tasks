cdbdc10b66c7da76629dcdcb1da8909db4215d4e
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-30 20:25:55 +0000

    Added code to demonstrate a simple dictionary.

---
+++ week7/dictionary_tasks.py
@@ -0,0 +1,13 @@
+# Activity 1: Simple Dictionary
+
+def pattern():
+    sequences = {}
+    sequences["Short Sequence"] = 3
+    sequences["Medium Sequence"] = 2
+    sequences["Long Sequence"] = 1
+    return sequences
+
+def run():
+    print(pattern())
+
+run()

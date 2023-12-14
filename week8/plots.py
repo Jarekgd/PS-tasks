f34d92f03084cfa7ff12ea3b1f3e4a0e9962a085
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-12-14 09:36:56 +0000

    Added code to display multiple plots.

--- week8/plots.py
+++ week8/plots.py
@@ -12,4 +12,37 @@ def run_task1():
     plt.ylabel("y values")
     display_line(x_values, y_values)
 
-run_task1()
+# run_task1()
+
+
+#################################################
+# Activity 2: Squares with Line Plot
+
+def small():
+    x = [3, 4, 4, 3, 3]
+    y = [4, 4, 3, 3, 4]
+    plt.plot(x, y, 'ro:')
+
+
+
+def medium():
+    x = [2, 5, 5, 2, 2]
+    y = [5, 5, 2, 2, 5]
+    plt.plot(x, y, 'gs--')
+
+
+
+def large():
+    x = [1, 6, 6, 1, 1]
+    y = [6, 6, 1, 1, 6]
+    plt.plot(x, y, 'bx-')
+
+
+
+def run_task2():
+    small()
+    medium()
+    large()
+    plt.show()
+
+run_task2()

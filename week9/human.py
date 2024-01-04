b5998895794fffd5ad5a23036b76c22cc6683a72
Jaroslaw Glod <emmart@poczta.onet.pl>
2024-01-04 14:58:33 +0000

    Updated classes human and robot with methods to display objects.

--- week9/human.py
+++ week9/human.py
@@ -1,3 +1,4 @@
+# Activity 1: Basic Class
 class Human:
     MAX_ENERGY = 100
 
@@ -9,7 +10,15 @@ class Human:
     def display(self):
         print(f"I am {self.name}")
 
+    def __repr__(self):
+        return f'human(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'Human {self.name} is {self.age} years old.'
+
 
 if __name__ == "__main__":
     human = Human()
     human.display()
+    print(repr(human))
+    print(human)
--- week9/robot.py
+++ week9/robot.py
@@ -1,15 +1,25 @@
+# Activity 1: Basic Class
 class Robot:
     MAX_ENERGY = 100
 
-    def __init__(self, name="Robot", age=0, energy=0):
+    def __init__(self, name="Robot", age=0, energy=MAX_ENERGY):
         self.name = name
         self.age = age
-        self.energy = energy
+        self.energy = 0
 
     def display(self):
         print(f"I am {self.name}")
+
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'Robot {self.name} is {self.age} years old.'
 
 
 if __name__ == "__main__":
     robot = Robot()
     robot.display()
+    print(repr(robot))
+    print(robot)
+

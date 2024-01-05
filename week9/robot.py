515c7c6fb49542bd31c10ccd5a3bafc6875fdbb0
Jaroslaw Glod <emmart@poczta.onet.pl>
2024-01-05 00:03:40 +0000

    Updated classes human and robot with instance methods.

--- week9/robot.py
+++ week9/robot.py
@@ -16,10 +16,35 @@ class Robot:
     def __str__(self):
         return f'Robot {self.name} is {self.age} years old.'
 
+    def grow(self):
+        self.age += 1
+        return self.age
+
+    def eat(self, amount):
+        if self.energy + amount < self.MAX_ENERGY:
+            self.energy += amount
+        else:
+            self.energy = self.MAX_ENERGY
+        return self.energy
+
+    def move(self, distance):
+        if self.energy - distance > 0:
+            self.energy -= distance
+        else:
+            self.energy = 0
+        return self.energy
+
 
 if __name__ == "__main__":
     robot = Robot()
     robot.display()
     print(repr(robot))
     print(robot)
+    print(robot.grow())
+    robot.grow()
+    print(f"One more time: {robot.grow()}")
+    print(f"Eating grow: {robot.eat(10)}")
+    print(f"Eating grow: {robot.eat(7)}")
+    print(f"After move: {robot.move(40)}")
+    print(f"Eating grow: {robot.eat(7)}")
 

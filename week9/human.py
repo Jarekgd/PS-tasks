9e2d6a59dc579016ddf33b9c062d2a28e5ab1062
Jaroslaw Glod <emmart@poczta.onet.pl>
2024-01-12 19:39:18 +0000

    Updated classes human and robot with instance methods.

--- week9/human.py
+++ week9/human.py
@@ -1,24 +1,49 @@
-# Activity 1: Basic Class
 class Human:
     MAX_ENERGY = 100
 
-    def __init__(self, name="Human", age=0, energy=MAX_ENERGY):
+    def __init__(self, name="Human", age=0):
         self.name = name
-        self.age = age
-        self.energy = energy
-
-    def display(self):
-        print(f"I am {self.name}")
+        self.age = int(age)
+        self.energy = Human.MAX_ENERGY
 
     def __repr__(self):
-        return f'human(name={self.name}, age={self.age})'
+        return f'human(name={self.name}, age={self.age}, energy={self.energy})'
 
     def __str__(self):
         return f'Human {self.name} is {self.age} years old.'
 
+    def display(self):
+        print(f"I am {self.name}")
+
+    def grow(self):
+        print(self.age + 1)
+
+    def eat(self, amount):
+        potential_energy = self.energy + amount
+        if potential_energy > Human.MAX_ENERGY:
+            self.energy = Human.MAX_ENERGY
+            return potential_energy - self.energy
+        else:
+            self.energy = potential_energy
+        return 0
+
+    def move(self, distance):
+        potential_energy = self.energy - distance
+        if potential_energy < 0:
+            self.energy = 0
+            return self.energy - abs(potential_energy)
+        else:
+            self.energy = potential_energy
+            return 0
+
 
 if __name__ == "__main__":
-    human = Human()
-    human.display()
-    print(repr(human))
+    human = Human("Jarek", 58)
     print(human)
+    print(repr(human))
+    human.move(10)
+    print(repr(human))
+    human.eat(5)
+    print(repr(human))
+    human.eat(20)
+    print(repr(human))
--- week9/robot.py
+++ week9/robot.py
@@ -1,25 +1,56 @@
 # Activity 1: Basic Class
 class Robot:
+    laws = "Protect, Obey and Survive"
+
     MAX_ENERGY = 100
 
-    def __init__(self, name="Robot", age=0, energy=MAX_ENERGY):
+    @staticmethod
+    def the_laws():
+        print(Robot.laws)
+
+    def __init__(self, name="Robot", age=0):
         self.name = name
         self.age = age
-        self.energy = 0
+        self.energy = Robot.MAX_ENERGY
+
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'Robot {self.name} is {self.age} years old and my energy is {self.energy}.'
 
     def display(self):
         print(f"I am {self.name}")
 
-    def __repr__(self):
-        return f'robot(name={self.name}, age={self.age})'
+    def eat(self, amount):
+        potential_energy = self.energy + amount
+        if potential_energy > self.MAX_ENERGY:
+            self.energy = self.MAX_ENERGY
+            return potential_energy - self.energy
+        else:
+            self.energy = self.energy
+            return 0
 
-    def __str__(self):
-        return f'Robot {self.name} is {self.age} years old.'
+    def grow(self):
+        self.age += 1
+
+    def move(self, distance):
+        potential_energy = self.energy - distance
+        if potential_energy < 0:
+            self.energy = 0
+            return self.energy - abs(potential_energy)
+        else:
+            self.energy = potential_energy
+            return 0
 
 
 if __name__ == "__main__":
     robot = Robot()
-    robot.display()
+    Robot.the_laws()
     print(repr(robot))
-    print(robot)
-
+    robot.move(10)
+    print(repr(robot))
+    robot.eat(5)
+    print(repr(robot))
+    robot.eat(20)
+    print(repr(robot))
